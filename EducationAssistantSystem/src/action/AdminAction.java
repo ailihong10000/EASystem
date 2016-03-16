22cecf97bf442176c44333886470c8124a2c4e2b
liujicheng <liujicheng@shine-soft.com>
2016-03-16 19:56:11 +0800

    1

--- EducationAssistantSystem/src/action/AdminAction.java
+++ EducationAssistantSystem/src/action/AdminAction.java
@@ -5,7 +5,7 @@ import com.opensymphony.xwork2.ActionSupport;
 public class AdminAction extends ActionSupport {
 	@Override
 	public String execute() throws Exception {
-		// TODO Auto-generated method stub
+		
 		return SUCCESS;
 	}
 
